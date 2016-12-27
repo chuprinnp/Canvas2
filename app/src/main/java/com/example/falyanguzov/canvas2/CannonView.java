01e323a93e76744be9134d82b5133f565dbc728c
Fedor Lyanguzov <fedor.lyanguzov@ya.ru>
2016-12-27 15:59:30 +0300

    Рисование картинок

--- app/src/main/java/com/example/falyanguzov/canvas2/CannonView.java
+++ app/src/main/java/com/example/falyanguzov/canvas2/CannonView.java
@@ -1,8 +1,11 @@
 package com.example.falyanguzov.canvas2;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -10,6 +13,7 @@ import android.view.View;
  * Created by falyanguzov on 27.12.2016.
  */
 public class CannonView extends View {
+    private final Bitmap p;
     private Paint paint = new Paint();
     private float x=1;
     private int direction = 1;
@@ -19,9 +23,12 @@ public class CannonView extends View {
     private boolean isFirstFrame = true;
     private float y;
 
+
     public CannonView(Context context) {
         super(context);
         paint.setColor(0xFF0000FF);
+        p = BitmapFactory.decodeResource(getResources(),
+                R.drawable.python);
     }
 
     @Override
@@ -41,6 +48,8 @@ public class CannonView extends View {
             direction *=-1;
         x += velocity*direction;
         canvas.drawCircle(x, y, h/4, paint);
+        Rect dst = new Rect(0, 0, h, h);
+        canvas.drawBitmap(p, null, dst, paint);
         invalidate();
     }
 
